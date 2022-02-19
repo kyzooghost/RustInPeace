2d4da95ac5f47948441cf5ff2029642c1dc4dde0
kyzooghost <jeffersonzjtang@gmail.com>
2022-02-20 06:58:22 +1100

    Part 1.3.10

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-// 1.3.10
-// Write a filter InfixToPostfix that converts an arithmetic expression from infix to postfix.
-// e.g. Infix x^y/(5*z)+2 => Postfix xy^5z*/2+
+#[allow(non_snake_case)]
 
-// Non-operator => Push onto main stack
-// Operator => Push onto scratch stack
-// 
-
-#[derive(Debug)]
-struct FixedCapacityStack<T> {
-  N: usize, // size
-  a: Vec<T>, // stack entries
+trait Stack<T> {
+  fn size(&self) -> usize;
+  fn isEmpty(&self) -> bool;
+  fn push(&mut self, item:T);
+  fn pop(&mut self) -> T;
+  fn new(cap: usize) -> Self;
+  fn toString(&mut self) -> String;
 }
 
-impl<T> FixedCapacityStack<T> {
-  pub fn new(cap: usize) -> Self {
+#[derive(Debug)]
+struct FixedCapacityStackOfStrings {
+  N: usize, // size
+  a: Vec<String>, // stack entries
+}
+
+#[derive(Debug)]
+struct FixedCapacityStackOfChars {
+  N: usize, // size
+  a: Vec<char>, // stack entries
+}
+
+impl Stack<char> for FixedCapacityStackOfChars {
+  fn new(cap: usize) -> Self {
     assert!(cap != 0, "cap cannot be 0");
-    FixedCapacityStack { N: 0, a: vec!['\0'; cap]}
+    FixedCapacityStackOfChars { N: 0, a: vec!['\0'; cap]}
   }
 
   fn size(&self) -> usize {
@@ -38,21 +47,107 @@ impl<T> FixedCapacityStack<T> {
     popped_element
   }
 
-  fn isFull(&self) -> bool {
-    self.N == self.a.len()
+  fn toString(&mut self) -> String {
+    let mut string = String::from("");
+    while !self.isEmpty() {
+      string.push(self.pop());
+    }
+    string.chars().rev().collect::<String>()
+  }
+}
+
+impl Stack<String> for FixedCapacityStackOfStrings {
+  fn new(cap: usize) -> Self {
+    assert!(cap != 0, "cap cannot be 0");
+    FixedCapacityStackOfStrings { N: 0, a: vec![String::new(); cap]}
   }
 
+  fn size(&self) -> usize {
+    self.N
+  }
+
+  fn isEmpty(&self) -> bool {
+    self.N == 0
+  }
+
+  fn push(&mut self, item: String) {
+    self.a[self.N] = item;
+    self.N = self.N + 1;
+  }
+
+  fn pop(&mut self) -> String {
+    let popped_element: String = self.a[self.N - 1].clone();
+    self.a[self.N - 1] = "".to_string();
+    self.N = self.N - 1;
+    popped_element
+  }
+
+  fn toString(&mut self) -> String {
+    let mut string = String::from("");
+    while !self.isEmpty() {
+      string.push(self.pop().chars().nth(0).expect("?Not a single character string"));
+    }
+    string.chars().rev().collect::<String>()
+  }
 }
+
+// 1.3.10
+// Write a filter InfixToPostfix that converts an arithmetic expression from infix to postfix.
+// e.g. Infix x^y/(5*z)+2 => Postfix xy^5z*/2+
+
+// Non-operator => Push onto main stack
+// Operator => Push onto scratch stack
+// Every 2 non-operator -> push operator onto stack
 
 fn main() {
   // Create stack
-  // let string1 = String::from("1+2)*3-4)*5-6)))");
-  // let string2 = String::from("((1+2)*((3-4)*(5-6)))");
+  let string1 = String::from("x^y/(5*z)+2");
+  let string2 = String::from("xy^5z*/2+");
   // assert!( infix(string1) == string2, "incorrect infix implementation");
   // println!("Successful infix");
 }
 
 fn InfixToPostfix(string: String) -> String {
   let mut stack = FixedCapacityStackOfChars::new(100);
-  "a".to_string()
+  let mut operatorStack = FixedCapacityStackOfChars::new(100);
+  let characters: Vec<char> = string.chars().collect();
+  let mut parenthesisCount:i32 = 0;
+
+  for c in characters {
+    // If variable, push onto stack
+    if !isOperator(c) && !isParenthesis(c) {
+      stack.push(c);
+
+      if stack.N >= 2 {stack.push(operatorStack.pop())}
+      continue;
+    }
+
+    if c == '(' {
+      parenthesisCount = parenthesisCount + 1;
+      continue;
+    }
+
+    if c == ')' {
+      parenthesisCount = parenthesisCount - 1;
+      continue;
+    }
+
+    if isOperator(c) {
+      operatorStack.push(c);
+      continue;
+    }
+  }
+
+  
+  stack.toString()
+}
+
+fn isOperator (character: char) -> bool {
+  if character == '+' || character == '-' || character == '*' || character == '/' || character == '^' {return true}
+  false
+}
+
+fn isParenthesis (character: char) -> bool {
+  if character == '(' || character == ')' {return true}
+  false
 }
