5bc9735688e1110904f5bedfdcbfbf7a0c3af756
kyzooghost <jeffersonzjtang@gmail.com>
2022-06-22 01:50:36 +1000

    did up to 2.5.2

---
+++ src/algorithm_exercises/Chapter 2/2.5/2.5.1.rs
@@ -0,0 +1,11 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
+// p366 Exercises
+
+// 2.5.1
+
+// Redundant to compare characters beyond the length of the smallest word, because they will be invalid anyway
+
+fn main() {
+}
