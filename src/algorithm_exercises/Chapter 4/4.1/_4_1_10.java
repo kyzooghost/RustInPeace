389e5a2ee71372a53783b31e4670284bd2268cc0
kyzooghost <jeffersonzjtang@gmail.com>
2022-09-18 16:17:18 +1000

    did 4.1.10

---
+++ src/algorithm_exercises/Chapter 4/4.1/_4_1_10.java
@@ -0,0 +1,10 @@
+// 4.1.10 
+
+// Prove that every connected graph has a vertex whose removal (including all adjacent edges) will not disconnect the graph, and write a DFS method that finds such a vertex. Hint : Consider a vertex whose adjacent vertices are all marked.
+
+// In a connected graph, every pair of vertex has an edge
+// So if you remove a vertex and all adjacent edges, every edge connecting vertices excluding the removed vertex, remains. Hence graph will remain connected
+
+public class _4_1_10 {
+    
+}
