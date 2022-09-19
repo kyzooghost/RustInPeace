b11868d5fedbd5ca689c1421bb7939483430f514
kyzooghost <jeffersonzjtang@gmail.com>
2022-09-19 15:10:56 +1000

    did 4.1.35

--- src/algorithm_exercises/Chapter 4/4.1/_4_1_35.java
+++ src/algorithm_exercises/Chapter 4/4.1/_4_1_35.java
@@ -1,5 +1,14 @@
-// A graph is biconnected if every pair of vertices is connected by two disjoint paths. An articulation point in a connected graph is a vertex that would disconnect the graph if it (and its adjacent edges) were removed. Prove that any graph with no articulation points is biconnected. Hint : Given a pair of vertices s and t and a path connecting them, use the fact that none of the vertices on the path are articulation points to construct two disjoint paths connecting s and t.
+// A graph is biconnected if every pair of vertices is connected by two disjoint paths. An articulation point in a connected graph is a vertex that would disconnect the graph if it (and its adjacent edges) were removed. Prove that any graph with no articulation points is biconnected. 
 
+// Hint : Given a pair of vertices s and t and a path connecting them, use the fact that none of the vertices on the path are articulation points to construct two disjoint paths connecting s and t.
+
+// Biconnected - every pair of vertices is connected by two disjoint paths (two paths between two vertices, that don't share an edge)
+// Articulation point - will disconnect the graph if it and adjacent edges were removed
+
+// If no articulation points
+// => Cannot remove any one point and lose connecting path between vertex
+// => Cannot have a single path between two vertices
+// => Must be bijoint or more
 
 public class _4_1_35 {
     
