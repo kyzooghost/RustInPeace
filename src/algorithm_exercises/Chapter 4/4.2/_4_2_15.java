de9881db299ed6d7352c08737a214ffd513cb4d3
kyzooghost <jeffersonzjtang@gmail.com>
2022-09-20 22:42:06 +1000

    did 4.2.17

---
+++ src/algorithm_exercises/Chapter 4/4.2/_4_2_15.java
@@ -0,0 +1,7 @@
+// What are the strong components of a DAG?
+
+// Strong components of a DAG are single vertices
+
+public class _4_2_15 {
+    
+}
---
+++ src/algorithm_exercises/Chapter 4/4.2/_4_2_17.java
@@ -0,0 +1,10 @@
+// True or false: The reverse post order of a graph’s reverse is the same as the post-order of the graph.
+
+// Should be true, should have the reverse topological sort
+
+// According to - https://raw.githubusercontent.com/reneargento/algorithms-sedgewick-wayne/master/src/chapter4/section2/Exercise20.txt
+// Not neccessarily
+
+public class _4_2_17 {
+    
+}
