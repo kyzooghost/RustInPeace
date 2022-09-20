65f61002ae55ad39dc149d83c8bfda946916b7e0
kyzooghost <jeffersonzjtang@gmail.com>
2022-09-20 18:53:00 +1000

    did 4.2.12

---
+++ src/algorithm_exercises/Chapter 4/4.2/_4_2_12.java
@@ -0,0 +1,8 @@
+// How many edges are there in the transitive closure of a digraph that is a simple directed path with V vertices and V–1 edges?
+
+// V * (V - 1) * 2
+// Essentially the transitive closure matrix is half filled
+
+public class _4_2_12 {
+    
+}
