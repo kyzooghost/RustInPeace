40e1e2cedbad1afe2cd0ebf6370dba477636a66f
kyzooghost <jeffersonzjtang@gmail.com>
2022-09-20 22:22:01 +1000

    did 4.2.13

---
+++ src/algorithm_exercises/Chapter 4/4.2/_4_2_13.java
@@ -0,0 +1,31 @@
+// Give the transitive closure of the digraph with ten vertices and these edges:
+
+/*
+
+    3->7 1->4 7->8 0->5 5->2 3->8 2->9 0->6 4->9 2->6 6->4
+
+
+3 -> 7 -> 8
+1 -> 4 => 9
+0 -> 5 -> 2 -> 9
+0 -> 6 -> 4 -> 9
+2 -> 6 -> 4 -> 9
+
+  0 1 2 3 4 5 6 7 8 9
+0 T   T   T T T     T   
+1   T     T         T  
+2     T   T   T     T 
+3       T       T T    
+4         T         T 
+5     T   T T T     T   
+6         T   T     T   
+7               T T   
+8                 T   
+9                   T 
+
+ */
+
+
+public class _4_2_13 {
+    
+}
