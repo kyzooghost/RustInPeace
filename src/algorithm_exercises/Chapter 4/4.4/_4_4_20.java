738dac8fd44e6f2b8e3cf1769a1983732e92a98a
kyzooghost <jeffersonzjtang@gmail.com>
2022-11-12 06:29:41 +1100

    did 4.4.20

---
+++ src/algorithm_exercises/Chapter 4/4.4/_4_4_20.java
@@ -0,0 +1,7 @@
+/*
+ * p686
+ * Find a currency-conversion table online or in a newspaper. Use it to build an arbitrage table. Note : Avoid tables that are derived (calculated) from a few values and that therefore do not give sufficiently accurate conversion information to be interesting. Extra credit : Make a killing in the money-exchange market!
+ */
+
+// See https://github.com/kyzooghost/arbitrage-scanner
+// Works in theory. In practice network latency is unacceptable for results, and requires much more optimization beyond implementing the arbitrage-finding algorithm.
