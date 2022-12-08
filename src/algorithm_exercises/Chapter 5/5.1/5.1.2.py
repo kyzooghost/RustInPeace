68beab2d2edf94603d59a3bef508fc411fbade16
kyzooghost <jeffersonzjtang@gmail.com>
2022-12-08 19:31:16 +1100

    did 5.1.2

---
+++ src/algorithm_exercises/Chapter 5/5.1/5.1.2.py
@@ -0,0 +1,108 @@
+# Give a trace for LSD string sort for the keys no is th ti fo al go pe to co to th ai of th pa
+
+
+# For each character, starting with least significant
+    # Frequency count
+    # Counts to indices
+    # Copy to aux array
+    # Copy back to original array
+
+"""
+
+no 
+is 
+th 
+ti 
+fo 
+al 
+go 
+pe 
+to 
+co 
+to 
+th 
+ai 
+of 
+th 
+pa
+
+2nd character
+    Frequency count
+        a = 1
+        e = 1
+        f = 1
+        h = 3
+        i = 2
+        l = 1
+        o = 6
+        s = 1
+    
+    Frequency to indices
+        a = 1
+        e = 2
+        f = 3
+        h = 6
+        i = 8
+        l = 9
+        o = 15
+        s = 16
+
+    Copy to aux array
+        pa
+        pe
+        of
+        th
+        th 
+        th 
+        ti 
+        ai
+        al
+        no 
+        fo 
+        go 
+        to 
+        co 
+        to 
+        is 
+
+1st character
+    Frequency count
+        a = 2
+        c = 1
+        f = 1
+        g = 1
+        i = 1
+        o = 1
+        n = 1
+        p = 2
+        t = 6
+
+    Count to indices
+        a = 2
+        c = 3
+        f = 4
+        g = 5
+        i = 6
+        o = 7
+        n = 8
+        p = 10
+        t = 16
+
+    Copy to aux array
+        ai
+        al
+        co 
+        fo 
+        go
+        is
+        of
+        no
+        pa
+        pe
+        th
+        th 
+        th 
+        ti 
+        to 
+        to 
+"""
